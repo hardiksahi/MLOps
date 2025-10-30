334c8a0549b8eed097c357e40dfb20e920e0d8cb
hardik.sahi <hardik.sahi@tap.work>
2025-10-30 19:00:31 -0400

    Changes for backend

--- tutorials/kedro/kedro_app_as_package/backend/src/main.py
+++ tutorials/kedro/kedro_app_as_package/backend/src/main.py
@@ -2,7 +2,8 @@
 # from kedro2package.__main__ import main
 # from typing import Dict
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse
 from typing import Dict
 
 # from kedro2package.__main__ import main
@@ -12,13 +13,15 @@ from typing import Dict
 import os
 import sys
 import pandas as pd
+from kedro.framework.project import configure_project
+from kedro.framework.session import KedroSession
+from kedro2package.hooks import RUNTIME_DATASETS_CTX
 
 extra_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(extra_path)
 from models.model import PipelineRequest
+from helper import is_list_of_dicts
 
-from kedro.framework.project import configure_project
-from kedro.framework.session import KedroSession
 
 app = FastAPI(title="Kedro application packaged as a python dependency")
 
@@ -34,14 +37,47 @@ async def get_status():
 @app.post("/run-kedro-pipeline", response_model=None)
 async def run_kedro_pipeline(
     request: PipelineRequest,
+    use_runtime_input: bool = Query(
+        False, description="Use in-memory DataFrame instead of on-disk dataset"
+    ),
 ) -> Dict[str, pd.DataFrame]:
-    # result_dict = main(["--pipeline", pipeline_name, "--env", environment])
-    # result_dict = run_kedro(pipeline_name=request.pipeline_name, env=environment)
 
-    # return_df = result_dict["model_input_table"].load()
-    # return {"df": return_df.shape}
+    if use_runtime_input and len(request.runtime_catalog_dict) > 0:
+        for dataset_name, dataset in request.runtime_catalog_dict.items():
+            if is_list_of_dicts(dataset):
+                RUNTIME_DATASETS_CTX.set({dataset_name: pd.DataFrame(dataset)})
+            else:
+                RUNTIME_DATASETS_CTX.set({dataset_name: pd.DataFrame(dataset)})
 
     with KedroSession.create(env=os.getenv("KEDRO_ENV", "base")) as session:
-        outputs = session.run(**request.params)
 
-    return {"status": "success", "outputs": list(outputs.keys())}
+        pipeline_outputs_dict = session.run(**request.run_params_dict)
+
+        pipeline_name = request.run_params_dict["pipeline_name"]
+        output_env_var = f"KEDRO_PIPELINE_{pipeline_name.upper()}_OUTPUT"
+        output_env_var_value = os.getenv(
+            output_env_var,
+            None,
+        )
+
+        return_dict = {}
+        if (
+            output_env_var_value is not None
+            and output_env_var_value in pipeline_outputs_dict
+        ):
+            return_obj = pipeline_outputs_dict[output_env_var_value].load()
+            if isinstance(return_obj, pd.DataFrame):
+                return_obj = return_obj.to_dict("records")
+                return_dict["output"] = JSONResponse(content=return_obj)
+            else:
+                return_dict["output"] = str(return_obj)
+
+            return_dict["output_name"] = output_env_var_value
+            return_dict["status"] = "success"
+        else:
+            return_dict["status"] = "fail"
+            return_dict["reason"] = f"{output_env_var} not present in env file"
+
+    return_dict["pipeline_name"] = pipeline_name
+
+    return return_dict
--- tutorials/kedro/kedro_app_as_package/backend/src/models/model.py
+++ tutorials/kedro/kedro_app_as_package/backend/src/models/model.py
@@ -7,7 +7,8 @@ class Request(BaseModel):
 
 
 class PipelineRequest(BaseModel):
-    params: dict = {"pipeline_name": "__default__"}
+    run_params_dict: dict = {"pipeline_name": "__default__"}
+    runtime_catalog_dict: dict = {}
 
 
 # class Response(BaseModel):
